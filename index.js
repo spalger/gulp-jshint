97407530b89078f825758d779e57fe20396c60e2
Contra <contra@maricopa.edu>
2013-12-01 11:49:46 -0700

    fix jshint reporter compatibility

--- index.js
+++ index.js
@@ -5,19 +5,37 @@
 var es = require('event-stream'),
   jshint = require('jshint').JSHINT;
 
+var formatOutput = function(success, file, opt) {
+  // no error
+  if (success) return {success:success};
+
+  // errors
+  var results = jshint.errors.map(function (err) {
+    if (!err) return;
+    return { file: file.path || "stdin", error: err };
+  }).filter(function (err) {
+    return err;
+  });
+
+  var data = [jshint.data()];
+  data[0].file = file.path || "stdin";
+
+  var output = {
+    success: success,
+    results: results,
+    data: data,
+    opt: opt
+  };
+
+  return output;
+};
+
 var gulpJSHint = function(opt){
   return es.map(function (file, cb) {
     var success = jshint(String(file.contents), opt);
 
     // send status down-stream
-    file.jshint = {
-      success: success,
-      results: jshint.errors || [],
-      data: [jshint.data()],
-      opt: opt
-    };
-    file.jshint.errorCount = file.jshint.results.length;
-    file.jshint.data[0].file = file.path;
+    file.jshint = formatOutput(success, file, opt);
 
     cb(null, file);
   });
@@ -38,10 +56,14 @@ gulpJSHint.reporter = function (reportWriter) {
   if (!reportWriter) {
     return gulpJSHint.reporterSimple();
   }
+
+  // load jshint reporter
+  // legacy jshint reporters get a shimmed format
   var rpt = require('jshint/src/reporters/'+reportWriter).reporter;
   if (!rpt) {
     throw new Error('invalid reporter: '+reportWriter);
   }
+
   return es.map(function (file, cb) {
     rpt(file.jshint.results, file.jshint.data, file.jshint.opt);
     return cb(null, file);
