690ea92260d142663b41beb91ab460427074182c
Tom Chen <developer@tomchentw.com>
2014-06-18 10:15:18 +0800

    feat(src/*): use nodejs styled callback

--- src/extract.js
+++ src/extract.js
@@ -7,20 +7,12 @@ module.exports = function extract(when) {
   when = when || 'auto';
 
   return through2.obj(function (file, enc, done) {
-    var stream = this;
-
     fileIgnored(file, function (err, ignored) {
-      if (err) {
-        stream.emit('error', err);
-        done();
-        return;
-      }
-      if (!ignored) {
+      if (!err && !ignored) {
         file.jshint = file.jshint || {};
         file.jshint.extracted = jshintcli.extract(file.contents.toString('utf8'), when);
       }
-      stream.push(file);
-      done();
+      done(err, file);
     });
   });
 };
