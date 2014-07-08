d299600f4b04422badf0cbd6a92435fa57cde49d
Spencer Alger <spencer@spenceralger.com>
2014-07-08 14:43:20 -0700

    simplified fail reporter, removing the buffer option

--- src/reporters/fail.js
+++ src/reporters/fail.js
@@ -1,36 +1,27 @@
 var stream = require('../stream');
 var PluginError = require('gulp-util').PluginError;
 
-module.exports = function (config) {
-  config = config || config;
-
+module.exports = function () {
   var fails = null;
-  var buffer = config.buffer === false ? null : [];
-
-  function error() {
-    return new PluginError('gulp-jshint', {
-      message: 'JSHint failed' + (fails ? ' for: ' + fails.join(', ') : ''),
-      showStack: false
-    });
-  }
+  var buffer = [];
 
   return stream(
     function through(file) {
       // check for failure
       if (file.jshint && !file.jshint.success && !file.jshint.ignored) {
         (fails = fails || []).push(file.path);
+        buffer = false;
       }
 
-      // if we failed, and we are not buffering files, emit error now
-      if (fails && !buffer) this.emit('error', error());
-
-      // push to either the buffer or the next stream
-      (buffer || this).push(file);
-    },
-    function flush() {
+      if (buffer) buffer.push(file);
+    }, function flush() {
       if (fails) {
-        this.emit('error', error());
-      } else if (buffer) {
+        this.emit('error', new PluginError('gulp-jshint', {
+          message: 'JSHint failed for: ' + fails.join(', '),
+          showStack: false
+        }));
+      }
+      else {
         // send on the buffered files
         buffer.forEach(function (file) {
           this.push(file);
