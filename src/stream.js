12b0fa51b3e8ebfec2733ee3f7133f3df4660f0f
Spencer Alger <spencer@spenceralger.com>
2014-07-08 14:43:35 -0700

    added a timeout param to the stream wraper

--- src/stream.js
+++ src/stream.js
@@ -1,13 +1,25 @@
 var through2 = require('through2');
 
-module.exports = function (handler, flush) {
+module.exports = function (config, handler, flush) {
+  if (typeof config === 'function') {
+    flush = handler;
+    handler = config;
+    config = {};
+  }
+
+  // default to a pass through stream
+  if (typeof handler !== 'function') handler = function (a) { this.push(a); };
+
+  var ms = (config && config.timeout) || 30000;
+
   // if a handler leaves off the done callback, we will call it for them
   var async = handler.length >= 2;
 
   var str = through2({ objectMode: true }, function (obj, enc, _cb) {
     var timeout;
     var done = function (err, obj) {
-      clearTimeout(timeout);
+      if (async) clearTimeout(timeout);
+
       if (err) str.emit('error', err);
       if (obj) str.push(obj);
 
@@ -17,8 +29,8 @@ module.exports = function (handler, flush) {
 
     if (async) {
       timeout = setTimeout(function () {
-        throw new Error('Failed to call done in a stream handler before 30sec timeout.');
-      }, 30000);
+        str.emit('error', new Error('Failed to call done in a stream handler before ' + ms + 'ms timeout.'));
+      }, ms);
       handler.call(str, obj, done);
     } else {
       handler.call(str, obj);
