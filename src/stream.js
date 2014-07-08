7d5c17b83ffa8749bd783322ae6ba3ae6b4a78d8
Spencer Alger <spencer@spenceralger.com>
2014-07-08 12:48:25 -0700

    the streams now use the plain through2() function so they can specify a flush handler if they want.

--- src/stream.js
+++ src/stream.js
@@ -1,17 +1,25 @@
-var through2ObjStr = require('through2').obj;
+var through2 = require('through2');
+
 module.exports = function (handler, flush) {
-  var str = through2ObjStr(function (obj, enc, _cb) {
-    function done(err, obj) {
+  // if a handler leaves off the done callback, we will call it for them
+  var async = handler.length >= 2;
+
+  var str = through2({ objectMode: true }, function (obj, enc, _cb) {
+    var done = function (err, obj) {
       if (err) str.emit('error', err);
       if (obj) str.push(obj);
-      _cb();
-    }
 
-    if (handler.length >= 2) handler.call(str, obj, done);
-    else {
+      _cb();
+    };
+
+
+    if (async) {
+      handler.call(str, obj, done);
+    } else {
       handler.call(str, obj);
       done();
     }
   }, flush);
+
   return str;
 };
